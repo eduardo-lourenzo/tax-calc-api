f085ba40464db7623f5713ced8416a4828224e94
Eduardo C. Lourenzo <eduardo.lourenzo@zup.com.br>
2025-03-05 08:44:31 -0300

    build: spring initializr

---
+++ src/main/java/br/edu/zup/tax_calc_api/TaxCalcApiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.zup.tax_calc_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxCalcApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxCalcApiApplication.class, args);
+	}
+
+}
