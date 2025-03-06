344d55ba55932bb96069ce74b83c69ddec59950b
Eduardo C. Lourenzo <eduardo.lourenzo@zup.com.br>
2025-03-06 14:23:00 -0300

    feat: add AllArgsConstructor annotation

--- src/main/java/br/edu/zup/tax_calc_api/dtos/RegisterResponseDTO.java
+++ src/main/java/br/edu/zup/tax_calc_api/dtos/RegisterResponseDTO.java
@@ -1,9 +1,11 @@
 package br.edu.zup.tax_calc_api.dtos;
 
 import br.edu.zup.tax_calc_api.models.RoleEnum;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class RegisterResponseDTO {
     private Long id;
     String username;
