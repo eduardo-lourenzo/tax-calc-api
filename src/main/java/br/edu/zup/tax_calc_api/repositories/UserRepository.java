a3d53426c6ba6bfc8d6971e4b0f183f8dccc8e30
Eduardo C. Lourenzo <eduardo.lourenzo@zup.com.br>
2025-03-06 09:37:10 -0300

    fix: add Repository annotation

--- src/main/java/br/edu/zup/tax_calc_api/repositories/UserRepository.java
+++ src/main/java/br/edu/zup/tax_calc_api/repositories/UserRepository.java
@@ -2,9 +2,11 @@ package br.edu.zup.tax_calc_api.repositories;
 
 import br.edu.zup.tax_calc_api.models.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByUsername(String username);
     Boolean existsByUsername(String username);
