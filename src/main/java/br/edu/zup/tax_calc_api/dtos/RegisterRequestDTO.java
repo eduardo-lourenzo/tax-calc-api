2982795ea5d68c312e9848f2b02b430e805807d9
Eduardo C. Lourenzo <eduardo.lourenzo@zup.com.br>
2025-03-06 14:49:37 -0300

    feat: add Validation annotations

--- src/main/java/br/edu/zup/tax_calc_api/dtos/RegisterRequestDTO.java
+++ src/main/java/br/edu/zup/tax_calc_api/dtos/RegisterRequestDTO.java
@@ -1,11 +1,18 @@
 package br.edu.zup.tax_calc_api.dtos;
 
 import br.edu.zup.tax_calc_api.models.RoleEnum;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class RegisterRequestDTO {
+    @NotBlank(message = "O nome de usuário é obrigatório.")
     private String username;
+
+    @NotBlank(message = "A senha é obrigatória.")
     private String password;
+
+    @NotNull(message = "O papel é obrigatório.")
     private RoleEnum role;
 }
