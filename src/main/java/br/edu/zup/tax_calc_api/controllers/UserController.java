0a7e72742184760a8a6541e31d525a6e31179856
Eduardo C. Lourenzo <eduardo.lourenzo@zup.com.br>
2025-03-11 12:03:33 -0300

    fix: add RequestBody annotation in RegisterRequestDTO

--- src/main/java/br/edu/zup/tax_calc_api/controllers/UserController.java
+++ src/main/java/br/edu/zup/tax_calc_api/controllers/UserController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,7 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<RegisterResponseDTO> register(@Valid RegisterRequestDTO registerRequestDTO) {
+    public ResponseEntity<RegisterResponseDTO> register(@Valid @RequestBody RegisterRequestDTO registerRequestDTO) {
         return  ResponseEntity.status(HttpStatus.CREATED).body(userService.register(registerRequestDTO));
     }
 }
