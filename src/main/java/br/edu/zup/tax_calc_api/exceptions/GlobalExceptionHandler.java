ed57f6112aed279067b019a96591e67dc4ba11a9
Eduardo C. Lourenzo <eduardo.lourenzo@zup.com.br>
2025-03-11 11:45:15 -0300

    feat: create handleValidationExceptions method

--- src/main/java/br/edu/zup/tax_calc_api/exceptions/GlobalExceptionHandler.java
+++ src/main/java/br/edu/zup/tax_calc_api/exceptions/GlobalExceptionHandler.java
@@ -2,14 +2,29 @@ package br.edu.zup.tax_calc_api.exceptions;
 
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+
+
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<List<String>> handleConstraintViolationException(ConstraintViolationException ex) {
         List<String> errors = new ArrayList<>();
