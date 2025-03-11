e9a677908fd1a449e3907c52a5408d222b6f2859
Eduardo C. Lourenzo <eduardo.lourenzo@zup.com.br>
2025-03-11 18:05:36 -0300

    feat: create TaxEntity class

---
+++ src/main/java/br/edu/zup/tax_calc_api/models/TaxEntity.java
@@ -0,0 +1,37 @@
+package br.edu.zup.tax_calc_api.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "taxes")
+public class TaxEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(value = AccessLevel.NONE)
+    private Long id;
+
+    @NotBlank(message = "O nome do imposto é obrigatório.")
+    @Column(nullable = false, unique = true, length = 63)
+    private String name;
+
+    @NotBlank(message = "A descrição imposto é obrigatória.")
+    @Column(nullable = false, length = 255)
+    private String description;
+
+    @Column(nullable = false, precision = 12, scale = 6)
+    @NotNull(message = "A alíquota é obrigatória.")
+    @Positive(message = "A alíquota deve ser maior que zero.")
+    private BigDecimal aliquot;
+}
+
