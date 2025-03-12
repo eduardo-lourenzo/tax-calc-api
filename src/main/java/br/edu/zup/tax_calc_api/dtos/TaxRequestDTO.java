078dadff9161baf53acbd3be4a7ad90df88702d7
Eduardo C. Lourenzo <eduardo.lourenzo@zup.com.br>
2025-03-12 14:13:50 -0300

    chore: remove imports

--- src/main/java/br/edu/zup/tax_calc_api/dtos/TaxRequestDTO.java
+++ src/main/java/br/edu/zup/tax_calc_api/dtos/TaxRequestDTO.java
@@ -1,16 +1,11 @@
 package br.edu.zup.tax_calc_api.dtos;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import lombok.AccessLevel;
 import lombok.Data;
-import lombok.Setter;
 
 import java.math.BigDecimal;
 
