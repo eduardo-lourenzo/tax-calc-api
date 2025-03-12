c3bf78147221727a01cee8ae4ba812b2a069681c
Eduardo C. Lourenzo <eduardo.lourenzo@zup.com.br>
2025-03-12 14:12:08 -0300

    feat: create TaxRequestDTO class

---
+++ src/main/java/br/edu/zup/tax_calc_api/dtos/TaxRequestDTO.java
@@ -0,0 +1,29 @@
+package br.edu.zup.tax_calc_api.dtos;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Data
+public class TaxRequestDTO {
+    @NotBlank(message = "O nome do imposto é obrigatório.")
+    private String name;
+
+    @NotBlank(message = "A descrição imposto é obrigatória.")
+    private String description;
+
+    @NotNull(message = "A alíquota é obrigatória.")
+    @Digits(integer = 6, fraction = 6, message = "Número inválido, máximo 6 inteiros e 6 decimais.")
+    @Positive(message = "A alíquota deve ser maior que zero.")
+    private BigDecimal aliquot;
+}
