3ed8869acacec411fb362824f83103649eb92c0b
Eduardo C. Lourenzo <eduardo.lourenzo@zup.com.br>
2025-03-13 14:03:58 -0300

    feat: add AllArgsConstructor Lombok annotation

--- src/main/java/br/edu/zup/tax_calc_api/dtos/CalcResponseDTO.java
+++ src/main/java/br/edu/zup/tax_calc_api/dtos/CalcResponseDTO.java
@@ -1,10 +1,12 @@
 package br.edu.zup.tax_calc_api.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
+@AllArgsConstructor
 public class CalcResponseDTO {
     private String typeTax;
     private BigDecimal baseValue;
