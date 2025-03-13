b3bc6c20d5e9464456481314c8cf8a844d4894b1
Eduardo C. Lourenzo <eduardo.lourenzo@zup.com.br>
2025-03-13 13:43:15 -0300

    fix: change return from CalcRequestDTO to CalcResponseDTO

--- src/main/java/br/edu/zup/tax_calc_api/services/CalcService.java
+++ src/main/java/br/edu/zup/tax_calc_api/services/CalcService.java
@@ -1,7 +1,8 @@
 package br.edu.zup.tax_calc_api.services;
 
 import br.edu.zup.tax_calc_api.dtos.CalcRequestDTO;
+import br.edu.zup.tax_calc_api.dtos.CalcResponseDTO;
 
 public interface CalcService {
-    CalcRequestDTO calculateTax(CalcRequestDTO calcRequestDTO);
+    CalcResponseDTO calculateTax(CalcRequestDTO calcRequestDTO);
 }
