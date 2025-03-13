3243236b0481d3b8cf1f8b2f674d13e8cfdf0ae7
Eduardo C. Lourenzo <eduardo.lourenzo@zup.com.br>
2025-03-13 11:20:37 -0300

    feat: create CalcRequestDTO class

---
+++ src/main/java/br/edu/zup/tax_calc_api/dtos/CalcRequestDTO.java
@@ -0,0 +1,21 @@
+package br.edu.zup.tax_calc_api.dtos;
+
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class CalcRequestDTO {
+
+    @NotNull(message = "O identificador do tipo do imposto é obrigatório.")
+    private Long typeTaxId;
+
+    //12 = 1 trillion - 1
+    @NotNull(message = "O valor base é obrigatório.")
+    @Digits(integer = 12, fraction = 2, message = "Valor base inválido, máximo 12 inteiros e 2 decimais.")
+    @Positive(message = "O valor base deve ser maior que zero.")
+    private BigDecimal baseValue;
+}
