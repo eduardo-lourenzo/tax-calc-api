f427b4cb6bed0964e8cf4562193f166e348f5b23
Eduardo C. Lourenzo <eduardo.lourenzo@zup.com.br>
2025-03-14 17:57:54 -0300

    refactor: add getRoleName method

--- src/main/java/br/edu/zup/tax_calc_api/models/RoleEnum.java
+++ src/main/java/br/edu/zup/tax_calc_api/models/RoleEnum.java
@@ -1,7 +1,10 @@
 package br.edu.zup.tax_calc_api.models;
 
-// todo GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + userEntity.getRole().name());
 public enum RoleEnum {
     ADMIN,
     USER;
+
+    public String getRoleName() {
+        return "ROLE_" + this.name();
+    }
 }
