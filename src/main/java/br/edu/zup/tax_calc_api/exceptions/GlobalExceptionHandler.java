beaf2aa4939f979ad0e142360c885eb6a908d118
Eduardo C. Lourenzo <eduardo.lourenzo@zup.com.br>
2025-03-14 17:53:53 -0300

    feat: create handleUsernameNotFoundException method

--- src/main/java/br/edu/zup/tax_calc_api/exceptions/GlobalExceptionHandler.java
+++ src/main/java/br/edu/zup/tax_calc_api/exceptions/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package br.edu.zup.tax_calc_api.exceptions;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -49,6 +50,11 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(TaxNotFoundException.class)
     public ResponseEntity<String> handleTaxNotFoundException(TaxNotFoundException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<String> handleUsernameNotFoundException(UsernameNotFoundException ex){
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 }
