d3db396cca580d08adbcabfe033bd60a0c3ad032
Eduardo C. Lourenzo <eduardo.lourenzo@zup.com.br>
2025-03-18 16:40:21 -0300

    fix: add Component annotation

--- src/main/java/br/edu/zup/tax_calc_api/security/KeyProvider.java
+++ src/main/java/br/edu/zup/tax_calc_api/security/KeyProvider.java
@@ -2,9 +2,11 @@ package br.edu.zup.tax_calc_api.security;
 
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
 
 import java.security.Key;
 
+@Component
 public class KeyProvider {
     private static final String SECRET_KEY = System.getenv("JWT_SECRET_KEY");
 
