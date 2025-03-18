ecf453d8d161076bedbaf75838dd2a5c7cae6e77
Eduardo C. Lourenzo <eduardo.lourenzo@zup.com.br>
2025-03-18 18:31:56 -0300

    teste: apply one hundred percent coverage test

--- src/test/java/br/edu/zup/tax_calc_api/security/KeyProviderTest.java
+++ src/test/java/br/edu/zup/tax_calc_api/security/KeyProviderTest.java
@@ -1,29 +1,25 @@
 package br.edu.zup.tax_calc_api.security;
 
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.security.Key;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
-@SpringBootTest
 class KeyProviderTest {
 
     private KeyProvider keyProvider;
 
     @BeforeEach
     void setUp() {
-        keyProvider = mock(KeyProvider.class);
+        keyProvider = new KeyProvider();
 
-        when(keyProvider.key()).thenReturn(
-                Keys.hmacShaKeyFor(Decoders.BASE64.decode("VGVzdGUgVGVzdGUgVGVzdGUgVGVzdGUgVGVzdGUgVGVzdGUgVGVzdGU"))
+        ReflectionTestUtils.setField(
+                keyProvider,
+                "secretKey",
+                "VGVzdGUgVGVzdGUgVGVzdGUgVGVzdGUgVGVzdGUgVGVzdGUgVGVzdGU"
         );
     }
 
@@ -31,6 +27,7 @@ class KeyProviderTest {
     void testKeyGeneration() {
         Key key = keyProvider.key();
 
+        assertNotNull(key, "The generated key should not be null");
         assertNotEquals(0, key.getEncoded().length, "The generated key should not be empty");
         assertTrue(key.getAlgorithm().contains("HmacSHA"), "The key algorithm should be HmacSHA");
     }
