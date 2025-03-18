c167563ada9d4808665bb11577e85055ac3ec0f0
Eduardo C. Lourenzo <eduardo.lourenzo@zup.com.br>
2025-03-18 09:55:45 -0300

    feat: add login method

--- src/main/java/br/edu/zup/tax_calc_api/controllers/UserController.java
+++ src/main/java/br/edu/zup/tax_calc_api/controllers/UserController.java
@@ -1,7 +1,10 @@
 package br.edu.zup.tax_calc_api.controllers;
 
+import br.edu.zup.tax_calc_api.dtos.LoginRequestDTO;
+import br.edu.zup.tax_calc_api.dtos.LoginResponseDTO;
 import br.edu.zup.tax_calc_api.dtos.RegisterRequestDTO;
 import br.edu.zup.tax_calc_api.dtos.RegisterResponseDTO;
+import br.edu.zup.tax_calc_api.services.LoginService;
 import br.edu.zup.tax_calc_api.services.UserService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -15,13 +18,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
     private final UserService userService;
+    private final LoginService loginService;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, LoginService loginService) {
         this.userService = userService;
+        this.loginService = loginService;
     }
 
     @PostMapping("/register")
     public ResponseEntity<RegisterResponseDTO> register(@Valid @RequestBody RegisterRequestDTO registerRequestDTO) {
         return  ResponseEntity.status(HttpStatus.CREATED).body(userService.register(registerRequestDTO));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@Valid @RequestBody LoginRequestDTO loginRequestDTO) {
+        return ResponseEntity.ok().body(loginService.login(loginRequestDTO));
     }
 }
