ebbab626c1af30891097c0281fe483afcdad1db5
Eduardo C. Lourenzo <eduardo.lourenzo@zup.com.br>
2025-03-19 08:15:05 -0300

    feat: translate json key to portuguese in Calc

--- src/main/java/br/edu/zup/tax_calc_api/dtos/CalcRequestDTO.java
+++ src/main/java/br/edu/zup/tax_calc_api/dtos/CalcRequestDTO.java
@@ -1,5 +1,6 @@
 package br.edu.zup.tax_calc_api.dtos;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
@@ -11,11 +12,13 @@ import java.math.BigDecimal;
 public class CalcRequestDTO {
 
     @NotNull(message = "O identificador do tipo do imposto é obrigatório.")
+    @JsonProperty("tipoImpostoId")
     private Long typeTaxId;
 
     //12 = 1 trillion - 1
     @NotNull(message = "O valor base é obrigatório.")
     @Digits(integer = 12, fraction = 2, message = "Valor base inválido, máximo 12 inteiros e 2 decimais.")
     @Positive(message = "O valor base deve ser maior que zero.")
+    @JsonProperty("valorBase")
     private BigDecimal baseValue;
 }
--- src/main/java/br/edu/zup/tax_calc_api/dtos/CalcResponseDTO.java
+++ src/main/java/br/edu/zup/tax_calc_api/dtos/CalcResponseDTO.java
@@ -1,5 +1,6 @@
 package br.edu.zup.tax_calc_api.dtos;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -8,8 +9,16 @@ import java.math.BigDecimal;
 @Data
 @AllArgsConstructor
 public class CalcResponseDTO {
+
+    @JsonProperty("tipoImposto")
     private String typeTax;
+
+    @JsonProperty("valorBase")
     private BigDecimal baseValue;
+
+    @JsonProperty("aliquota")
     private BigDecimal aliquot;
+
+    @JsonProperty("valorImposto")
     private BigDecimal taxValue;
 }
